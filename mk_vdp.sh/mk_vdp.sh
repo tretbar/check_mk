4a495ddc90c26e84f497be425c31ef76ddd2f603
Tom <thomas.tretbar@gmx.de>
2017-03-22 16:58:38 +0100

    1.1

---
+++ mk_vdp.sh/mk_vdp.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# ttr@mh
+# 1.0   2016     - initial
+# 1.1   201703   - suppress WARN when age older 2d (ERR only)
+#
+# 1. install script in VDP appliance: /usr/lib/check_mk_agent/plugins/600
+# 2. /etc/sudoers:
+#    check_mk ALL = (root) NOPASSWD: /usr/local/avamar/bin/mccli
+#
+#
+# for VDP appliance maintenance put all services "VDP" of affected VMs in check_mk Downtime
+#
+#
+# - scripts generates one check per VM (all that are protected by jobs of this local appliance)
+#
+# - states:
+#   OK:   - check age less than 5min
+#         - and - errorcode for last VM backup is 0
+#         - and - last backup not older 90000secs (25h)
+#   WARN: - last backup older 90000secs (25h)
+#   CRIT  - last backup older 176400sec (49h)
+#         - or - errorcode for last VM backup is not 0
+#   UNKN: - check age older 5min
+#
+# - perfstat:
+#         - transferred bytes
+#         - duration in seconds
+#
+# - example status detail:
+#         OK - vSphere Data Protection Backup, VM: de-bx-srv-1009, Appliance: de-bx-srv-1027, last run: 2017-01-05 18:36 CET (status: Completed, transferred 1.4% of 73.5GB)
+#
+# - uses avamar cli:
+# #: mccli activity show
+#0,23000,CLI command completed successfully.
+#ID               Status    Error Code Start Time           Elapsed     End Time             Type             Progress Bytes New Bytes Client         Domain
+#
+#
+
+
+# get last activity, ignore table headers and uninteresting columns
+MCCLIOUT=`sudo /usr/local/avamar/bin/mccli activity show | awk '/^[0-9]* / { print $2" "$3" "$7" "$8" "$9" "$10" "$13" "$14" "$15" "$16 }' | sed 's/<0.05%/0/g; s/%//g'`
+
+# get list of VMs in activity log
+VMLIST=`echo "${MCCLIOUT}" | awk '{print $10}' | sort -u`
+
+# store current time for calculating backup age
+NOWSECONDS=`date +%s`
+
+# loop thru the list
+echo "${VMLIST}" | while read VM
+do
+
+  # get the VM lines, extract the most recent one:
+  # line format: "Completed 0 2016-12-17 17:01 CET 00h:02m:55s 8.0 GB 0 de-bx-srv-1001"
+  echo "${MCCLIOUT}" \
+  | egrep ' '${VM}'$' \
+  | sort -k 4,5 \
+  | tail -1 \
+  | while read STATUS ERRORCODE DURATION ENDDATE ENDTIME ENDTZ SIZE UNIT CHANGEDPERCENTAGE REST
+  do
+
+    unset $ERR
+    unset $WARN
+
+    EXITCODE=0
+
+    # check errorcode: ToDo adjust codes
+    case ${ERRORCODE} in
+      0)
+        #
+      ;;
+      *)
+        ERR=", mccli-errorcode="${ERRORCODE}"(!!)"
+      ;;
+    esac
+
+    # calculate age of last backup
+    ENDSECONDS=`date +%s -d "${ENDDATE} ${ENDTIME} ${ENDTZ}"`
+    let DELTA=$NOWSECONDS-$ENDSECONDS
+
+    if [ ${DELTA} -gt 90000 ]
+    then
+      if [ ${DELTA} -gt 176400 ]
+      then
+         ERR=${ERR}", ERROR: Backup older 2d (!!)"
+      else
+         WARN=${WARN}", WARN: Backup older 1d (!)"
+      fi
+    fi
+
+    # calculate duration in seconds for perfstat
+    DURATIONSEC=`echo ${DURATION} | sed 's/h:/ * 3600 + /; s/m:/ * 60 + /; s/s//' | bc`
+
+    [ -z "${WARN}" ] || EXITCODE=1
+    [ -z "${ERR}" ] || EXITCODE=2
+
+
+    # check_mk local check piggyback
+    echo "<<<<"${VM}">>>>"
+    echo "<<<local>>>"
+    echo ${EXITCODE}" VDP seconds="${DURATIONSEC}"|deltapercentage="${CHANGEDPERCENTAGE}" vSphere Data Protection Backup, VM: "${VM}", Appliance: "`hostname`", last run: "${ENDDATE}" "${ENDTIME}" "${ENDTZ}" (status: "${STATUS}", transferred "${CHANGEDPERCENTAGE}"% of "${SIZE}${UNIT}")"${ERR}${WARN}
+
+
+  done
+
+  echo
+done
